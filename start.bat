ea3fa264e2b712b00f8d5ae6ec0e48400fe824de
fayamonkey <fayamonkeyrecords@gmail.com>
2025-04-22 12:53:11 +0200

    Add start.bat

---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo off 
+title Eberle Artikel-Matching 
+echo Aktiviere virtuelles Environment... 
+call venv\Scripts\activate.bat 
+echo Starte Eberle Artikel-Matching... 
+python launcher.py 
+pause 
